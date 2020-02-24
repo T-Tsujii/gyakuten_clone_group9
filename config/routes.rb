e7c35177875d54d22bcc6b2872b548aeb9e39dc2
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-02-24 16:31:27 +0900

    コンフリクトの解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
   root 'movies#index'
   devise_for :users
   resources :users
->>>>>>> master
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_02_21_092911) do
-=======
-ActiveRecord::Schema.define(version: 2020_02_19_105901) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -35,9 +30,6 @@ ActiveRecord::Schema.define(version: 2020_02_19_105901) do
   end
 
   create_table "admin_users", force: :cascade do |t|
-=======
-  create_table "users", force: :cascade do |t|
->>>>>>> master
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -45,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_02_19_105901) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
@@ -56,10 +47,18 @@ ActiveRecord::Schema.define(version: 2020_02_19_105901) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-=======
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> master
   end
 
 end
