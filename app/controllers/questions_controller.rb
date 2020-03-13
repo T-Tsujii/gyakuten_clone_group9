1b0e33edccdee1650e2ddd4c329c3eb9de8a1873
skell <uchiyamaryou@RyosMacBook-Pro.local>
2020-03-13 20:21:08 +0900

    add form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,14 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def new
+    @question = Question.new
+  end
+
   def create
     Question.create(question_params)
   end
 
-  def new
-    @question = Question.new
-  end
 
   private
     def question_params
