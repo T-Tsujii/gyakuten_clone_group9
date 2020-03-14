bc40ed7e6ef912da0b579278db3f53facf71ce1a
skell <uchiyamaryou@RyosMacBook-Pro.local>
2020-03-14 22:11:55 +0900

    add flash order

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 
   def index
-    @questions = Question.all
+    @questions = Question.all.order(created_at: :"DESC")
     @question = Question.new
   end
 
@@ -11,6 +11,9 @@ class QuestionsController < ApplicationController
 
   def create
     Question.create(question_params)
+
+    flash[:notice] = "登録完了しました！"
+    redirect_to action: 'index'
   end
 
   private
