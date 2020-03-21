07a59112a490bdf5812d2127db76995011b41cee
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-03-21 17:29:33 +0900

    solutionsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'solutions/index'
+  get 'solutions/new'
+  get 'solutions/create'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'movies#index'
